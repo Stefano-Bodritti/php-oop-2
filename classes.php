9a7a4157941b9315d2781efcacbadab41e7a9146
Stefano <tepo@hotmail.it>
2021-05-12 16:18:23 +0200

    Creazione sottoclassi e relativi prodotti

--- classes.php
+++ classes.php
@@ -1,6 +1,5 @@
 <?php
-class Product
-{
+class Product {
   // attributi
   public $name;
   public $ID;
@@ -17,4 +16,32 @@ class Product
 
 }
 
-$libro = new Product("Il Codice da Vinci", 2345676, 9.90, "Un mistero che potrebbe scuotere le fondamenta del Cristianesimo si cela tra le opere di Leonardo. Il professore Langdon parte alla ricerca della scomoda verità assieme ad una criptologa francese.");
+class Book extends Product {
+  // attributi
+  public $pages;
+  public $edition;
+
+  // costruttore
+  public function __construct($_name, $_ID, $_price, $_description, $_pages, $_edition) {
+    parent::__construct($_name, $_ID, $_price, $_description);
+    $this->pages = $_pages;
+    $this->edition = $_edition;
+  }
+}
+
+class DVD extends Product {
+  // attributi
+  public $runningTime;
+  public $numberOfDisks;
+
+  // costruttore
+  public function __construct($_name, $_ID, $_price, $_description, $_runningTime, $_numberOfDisks)
+  {
+    parent::__construct($_name, $_ID, $_price, $_description);
+    $this->runningTime = $_runningTime;
+    $this->numberOfDisks = $_numberOfDisks;
+  }
+}
+
+$libro = new Book("Il Codice da Vinci", 2345676, 9.90, "Un mistero che potrebbe scuotere le fondamenta del Cristianesimo si cela tra le opere di Leonardo. Il professore Langdon parte alla ricerca della scomoda verità assieme ad una criptologa francese.", 689, "quinta");
+$DVD1 = new DVD("The Prestige", 9876546, 7.99, "La rivalità tra due illusionisti nell'epoca vittoriana non si ferma davanti a nulla. Nella loro battaglia per scoprire l'uno i segreti dell'altro contrappongono audacia e desiderio, abilità nell'intrattenere il pubblico e scienza, ambizione e amicizia, con risultati pericolosi e mortali.", 135, 1);
--- index.php
+++ index.php
@@ -4,22 +4,31 @@ require "classes.php";
 
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+  </head>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OOP</title>
-</head>
-
-<body>
-  <h2>Categoria Libri</h2>
-  <ul>
-    <li><?php echo $libro->name; ?></li>
-    <li><?php echo $libro->ID; ?></li>
-    <li><?php echo $libro->price; ?> euro</li>
-    <li><?php echo $libro->description; ?></li>
-  </ul>
-</body>
-
+  <body>
+    <h2>Categoria Libri</h2>
+    <ul>
+      <li><?php echo $libro->name; ?></li>
+      <li><?php echo $libro->ID; ?></li>
+      <li><?php echo $libro->price; ?> euro</li>
+      <li><?php echo $libro->description; ?></li>
+      <li><?php echo $libro->pages; ?> pagine</li>
+      <li><?php echo $libro->edition; ?> edizione</li>
+    </ul>
+    <h2>Categoria DVD</h2>
+    <ul>
+      <li><?php echo $DVD1->name; ?></li>
+      <li><?php echo $DVD1->ID; ?></li>
+      <li><?php echo $DVD1->price; ?> euro</li>
+      <li><?php echo $DVD1->description; ?></li>
+      <li><?php echo $DVD1->runningTime; ?> minuti</li>
+      <li>dischi: <?php echo $DVD1->numberOfDisks; ?></li>
+    </ul>
+  </body>
 </html>
